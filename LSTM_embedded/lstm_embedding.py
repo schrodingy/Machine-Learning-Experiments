39601cad86c59a613fd1da6e183627dfd8217a16
devenmithal <devenmithal@gmail.com>
2019-06-30 17:30:25 -0500

    Start LSTM experiment

---
+++ LSTM_embedded/lstm_embedding.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+
+def main():
+    df = read_csv()
+    result = lstm_train(df)
+
+
+def read_csv():
+    pass
+
+
+def lstm_train(df):
+    pass
+
+
+if __name__ == "__main__":
+  main()
