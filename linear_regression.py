f30b69cf000c657390b6ecd92ca2758ea6b88086
Deven Mithal <devenmithal@gmail.com>
2019-07-22 23:32:06 -0500

    Flesshed out comments

--- linear_regression.py
+++ linear_regression.py
@@ -1,3 +1,10 @@
+'''
+The linear regression method, as described in Stanford CS229, is equivalent to the
+iterative methods for solving linear systems as described in Chapter 7 of
+Numerical Methods by Ascher and Grief. In particular, the gradient descent methods
+described are equivalent, although CS229 does not cover conjugate gradient descent.
+'''
+
 def main():
     print("Here")
 
